7cac0c9649c57e090aaccf6468673aba0959d8bc
Peter Lang <peter.lang@mail.de>
2026-04-30 16:57:40 +0200

    for loop mini-workshop

--- 30-04-2026_for_loops.py
+++ 30-04-2026_for_loops.py
@@ -13,13 +13,16 @@
 # Gib beide Listen nochmals aus.
 # Fuege nochmals Butter und Brot zu meine_einkaufsliste hinzu. Was passiert?
 
+# Hinweis: Kaufe prueft hier auch, ob genug Geld vorhanden ist. 
+#          Nur wenn noch genug Geld vorhanden ist, fuegt es das Produkt zur Einkaufsliste hinzu.
+
 # Erster Teil: Ausgabe for-loop des Strings 'abc'
 def print_all(eine_liste):
   """
   Gibt Eintraege der eine_liste aus. Pro Zeile ein Eintrag.
   """
   for item in eine_liste:
-    print(f"Eintrag der Liste: {item}")
+    print(f"Eintrag der Liste: {index} {item}")
   return
 
 # Einkaufsliste
@@ -31,27 +34,45 @@ def drucke_einkaufsliste(einkaufsliste):
     print(f"Kaufe: {item}")
   return
 
-def kaufe(produkt, einkaufsliste):
+def kaufe(produkt, einkaufsliste, geld):
   """
   Fuegt der einkaufsliste das produkt hinzu und gibt neue Liste zurueck.
   """
-  return einkaufsliste + [produkt]
+  einkaufsliste2 = einkaufsliste
+  if produkt == 'Butter' and geld >= 60: 
+    geld = geld - 60
+    einkaufsliste2 = einkaufsliste + [produkt] 
+  if produkt == 'Brot' and geld >= 60: 
+    geld = geld - 60
+    einkaufsliste2 = einkaufsliste + [produkt] 
+  return einkaufsliste2, geld
 
 ###############################################################################
 
 if __name__ == '__main__':
 
-#  print_all('abc')
+  #print_all('abc')
 
   meine_einkaufsliste = ['Tee', 'Kaffee']
   eine_andere_einkaufsliste = ['Tee', 'Kaffee']
+  geld = 130
 
-#  drucke_einkaufsliste(meine_einkaufsliste)
-#  meine_einkaufsliste = kaufe('Butter', meine_einkaufsliste)
-#  meine_einkaufsliste = kaufe('Brot', meine_einkaufsliste)
-#  drucke_einkaufsliste(meine_einkaufsliste)
-#  drucke_einkaufsliste(eine_andere__einkaufsliste)
+  #drucke_einkaufsliste(meine_einkaufsliste)
+  #meine_einkaufsliste, geld = kaufe('Butter', meine_einkaufsliste, geld)
+  #meine_einkaufsliste, geld = kaufe('Brot', meine_einkaufsliste, geld)
+
+  # Oder in kurz um direkt das Produkt hinzuzufuegen 
+  #meine_einkaufsliste.append('Butter')
+  #meine_einkaufsliste.append('Brot')
+
+  #drucke_einkaufsliste(meine_einkaufsliste)
+  #drucke_einkaufsliste(eine_andere_einkaufsliste)
 
 # Es gibt auch Steuerungsmoeglichkeiten fuer einen Loop:
 # continue: gehe direkt zum naechsten Element und ueberspringe den restlichen Loop
 # break: beende die Loop Ausfuehrung fuer alle Elemente und mache mit anderem Code weiter
+# enumerate:
+#    for index, item in enumerate(['a', 'b', 'c']):
+#  Das enumerate zaehlt euch automatisch hoch. So koennt ihr sagen: Dieser Loop soll nur dreimal laufen: if index >= 3: break
+#  Er zaehlt von 0 los, index == 3 ist also die vierte Iteration. Und es ist wichtig, 2 Variablen zu nutzen, also hier index und item.
+#  for item in enumerate(...) wird einen Fehler verursachen. Die erste Variable ist der Zaehler von enumerate und die zweite das Element der Liste.
